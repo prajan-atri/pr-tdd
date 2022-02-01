3c1791b872f93331f83bf525ff7a4cb7faff05ad
prajan-atri <rajanp@atrihub.io>
2022-02-01 07:56:41 -0800

    feat(tdd-tutorial): Chapter 7 Adjusting the models and Final refactoring.
    [ICTRAINING-984]

--- lists/urls.py
+++ lists/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import re_path, include
-from lists import views as list_views
-from lists import urls as list_urls
+from django.urls import re_path
+from lists import views
+
+"""urlpatterns = [
+    re_path(r'^$', views.home_page, name='home'),
+    re_path(r'^lists/new$', views.new_list, name='new_list'),
+    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+]"""
 
 urlpatterns = [
-    re_path(r'^$', list_views.home_page, name='home'),
-    re_path(r'^lists/', include(list_urls)),
+    re_path(r'^new$', views.new_list, name='new_list'),
+    re_path(r'^(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 ]
